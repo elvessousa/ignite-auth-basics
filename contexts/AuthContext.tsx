37d2e09e9204e2611c3c811030ddf8bc5a071c09
Elves Sousa <elves.sousa@gmail.com>
2021-07-31 18:22:12 -0300

    Chapter IV: JWT and tokens with cookies

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,77 @@
+import Router from 'next/router';
+import { parseCookies, setCookie } from 'nookies';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { 'auth-basics.token': token } = parseCookies();
+    if (token) {
+      api.get('me').then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, 'auth-basics.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+
+      setCookie(undefined, 'auth-basics.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+
+      setUser({ email, permissions, roles });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      Router.push('/dashboard');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
